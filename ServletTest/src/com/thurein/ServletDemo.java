817caa55585517a038f2b5175cdaaa14feb52a4d
ThureinTun <thureintun@inspiron-3558.lan>
2020-10-09 19:12:34 +0630

    learning servlet

---
+++ ServletTest/src/com/thurein/ServletDemo.java
@@ -0,0 +1,59 @@
+package com.thurein;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletDemo
+ */
+@WebServlet("/myapp")
+public class ServletDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletDemo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String name=request.getParameter("name");
+		String dob=request.getParameter("dob");
+		String gender=request.getParameter("gender");
+		String [] course=request.getParameterValues("course");
+		
+		PrintWriter out=response.getWriter();
+		
+		out.println("Name :" + name);
+		out.println("DOB :" + dob);
+		out.println("Gender :" + gender);
+		
+		
+		
+		for(int i=0;i<course.length;i++) {
+			out.println("Subject"+(i+1) + " : "+ course[i]);
+		
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
